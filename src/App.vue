ea073fb4fff35c4bd65f43b6c7bf69604c408386
raloubs <lobonala@privatam.com>
2021-02-28 15:15:54 +0100

    Prepare project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <meteo msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Meteo from './components/Meteo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Meteo
   }
 }
 </script>
